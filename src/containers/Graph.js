6b2b148ea7946763766cce6d55b38fa86cac7334
Alonso Tagle <alonso.juarez@konfio.mx>
2017-08-08 19:49:18 -0500

    [Add] Chart

--- src/containers/Graph.js
+++ src/containers/Graph.js
@@ -1,14 +1,39 @@
 import React, { Component } from 'react';
-import { TaskList } from '../components';
+import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
+var BarChart = require('react-d3-basic').BarChart;
 
 
-export default class Graph extends Component {
+class Graph extends Component {
 
   render() {
     return (
       <div>
-        <TaskList />
+        {this.props.tasks.length > 0 ?
+          <BarChart
+            title="Completed tasks chart"
+            data= {this.props.tasks}
+            width= {700}
+            height= {500}
+            chartSeries= {[{field: 'state', name: 'Completed',}]}
+            x = {element => element.duration}
+            xLabel={"Tasks"}
+            xScale="ordinal"
+            yTicks={[1, ""]}
+            yLabel ={"State"}
+            />
+          :
+          <b>No Tasks</b>
+        }
       </div>
     );
   }
 }
+
+const mapStateToProps = state => {
+  return {
+    tasks: state.tasks,
+  }
+}
+
+export default withRouter(connect(mapStateToProps)(Graph));
